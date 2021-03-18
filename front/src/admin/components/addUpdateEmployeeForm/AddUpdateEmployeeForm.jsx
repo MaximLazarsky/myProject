a91efc861c42e650e8774945d32ccc15e76aced4
Maxim Lazarskiy <lazarskiy@gmail.com>
2021-03-18 14:44:53 +0200

    with AddUpdate page

--- front/src/admin/components/addUpdateEmployeeForm/AddUpdateEmployeeForm.jsx
+++ front/src/admin/components/addUpdateEmployeeForm/AddUpdateEmployeeForm.jsx
@@ -1,24 +1,43 @@
-import {TextField, Button} from '@material-ui/core';
+import {TextField, Button, Avatar} from '@material-ui/core';
+import useStyles from "./addUpdateEmployeeFormStyles"
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
-export default function AddUpdateEmployeeForm() {
+export default function AddUpdateEmployeeForm({employee}) {
+    const classes = useStyles()
     return(
-    <form>
-         <TextField
+    <form className={classes.addUpdateForm}>
+        <Avatar alt="avatar" className={classes.employeeAvatar}>
+            <PersonOutlineIcon/>
+        </Avatar>
+        <TextField
+            className={classes.inputAddUpdateForm}
+            label="Avatar"
+            variant="outlined"
+            type="file"
+        />
+        <TextField
+            className={classes.inputAddUpdateForm}
             label="Name"
             variant="outlined"
         />
         <TextField
+            className={classes.inputAddUpdateForm}
             label="Surname"
             variant="outlined"
         />
         <TextField
+            className={classes.inputAddUpdateForm}
             label="Position"
             variant="outlined"
         />
         <TextField
+            className={classes.inputAddUpdateForm}
             label="Expiriense"
             variant="outlined"
         />
+        <Button variant="outlined" color="primary" className={classes.addUpdateButton}> 
+           {employee ? "update employee" : "add employee"}
+        </Button>
     </form>
     )
 }
