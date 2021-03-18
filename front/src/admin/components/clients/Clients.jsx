116be1ee34898fc9f23b7a9cb2994e83709e4ce9
Maxim Lazarskiy <lazarskiy@gmail.com>
2021-03-18 15:13:21 +0100

    client page with styles

--- front/src/admin/components/clients/Clients.jsx
+++ front/src/admin/components/clients/Clients.jsx
@@ -5,6 +5,7 @@ import ClientItem from './clientItem/ClientItem'
 
 export default function Clients({clients}) {
 	const classes = useStyles()
+
 	return (
 	<div className={`container ${classes.clientsContainer}`}>
 	<Typography variant="h4" color="primary" className={classes.title} > List of Clients: </Typography>
--- front/src/admin/components/clients/clientItem/ClientItem.jsx
+++ front/src/admin/components/clients/clientItem/ClientItem.jsx
@@ -1,23 +1,39 @@
 import {Paper} from '@material-ui/core';
 import useStyles from './clientItemStyles';
+import {IconButton, Avatar} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default function ClientItem ({client}) {
-console.log(client)
+console.log(client.projects.length)
 	const classes = useStyles()
 	return (
 		<Paper className={classes.clientItem}>
-			<div>
-				<div>
-					{client.clientName}
-				</div>	
-				<div>
-					{client.clientSurname}
+			<div className={classes.clientData}>
+				<div className={`${classes.clientDataItem} ${classes.clietDataLeftPosition}`}>
+					<div>
+						{client.clientName}
+					</div>	
+					<div>
+						{client.clientSurname}
+					</div>
 				</div>
-				<div>
+				<div className={`${classes.clientDataItem} ${classes.clietDataCenterPosition}`}>
 					{client.clientEmail}
 				</div>
+				<div className={`${classes.clientDataItem} ${classes.clietDataRightPosition}`}> 
+					{client.projects.length ? client.projects.map((project) => <div key={project._id}>{project.projectName}</div>):<div>No projects</div>}
+				</div>
+				
 			</div>
-            <hr/>
+			<div className={classes.clientButtons}>
+                    <IconButton color="primary">
+                        <EditIcon />
+                    </IconButton>
+                    <IconButton color="primary">
+                        <DeleteIcon />
+                    </IconButton>
+                </div>
         </Paper>
 		
 	)
